400f8425e6f2cd924c076c15519c8025970549dd
Eglantine Schmitt <eglantine@cit.io>
2021-02-27 15:39:05 +0100

    initial commit

---
+++ R/auth.R
@@ -0,0 +1,139 @@
+library(httr)
+library(jsonlite)
+
+source("credentials.conf")
+
+####################################
+
+setCredentials = function(login, password){
+  credentials = paste("Basic", jsonlite::base64_enc(paste0(login,":",password)))
+}
+
+getSessionId = function(login = "eglantine@cit.io", password, group = "casablanca", env = "staging"){
+  if(is.null(login)||is.null(password)){
+    stop("Please provide login and password")
+  } else {
+
+    credentials = setCredentials(login, password)
+
+    login_route = paste0(
+      "https://",
+      group,
+      ".gateway.",
+      ifelse(env=="staging","staging.",""),
+      "cit.io/api/login"
+    )
+
+    response = GET(login_route, add_headers(Authorization = credentials))
+
+    return(
+       response$cookies$value[response$cookies$name=="sessionid"]
+    )
+  }
+}
+
+buildBaseUrl = function(group, env) {
+  base_url = paste0(
+    "https://",
+    group,
+    ".api.",
+    ifelse(env=="staging","staging.",""),
+    "cit.io"
+  )
+
+  return(base_url)
+}
+
+getResponseFromRoute = function(route_url, session_id){
+  response = GET(route_url,set_cookies(sessionid = session_id))
+  return(
+    content(response)
+  )
+}
+
+getAgencyId = function(base_url, session_id){
+  agency_route = paste0(base_url, "/rest/agency")
+  response = getResponseFromRoute(agency_route,session_id)
+
+  if(is.null(response$id)){
+    stop("Agence non disponible")
+  } else {
+    return(response$id)}
+}
+
+
+getReferentialSection = function(base_url,session_id, referential_section){
+  referential_route = paste0(base_url,"/rest/", referential_section)
+  response = getResponseFromRoute(referential_route, session_id)
+  #referential_table = do.call(rbind.data.frame, c(response, stringsAsFactors = F))
+  referential_table = data.table::rbindlist(response, fill = TRUE)
+
+  return(referential_table)
+}
+
+getKPIdata = function(base_url, kpi, agency_id, spatial_aggregation_level = "line", aggregated_by_time = FALSE,aggregated_by_day = FALSE, start_date = Sys.Date() - 7, end_date = Sys.Date(), days_of_the_week =  1111111, session_id){
+
+  kpi_base_url = paste(base_url,
+                       "kpis",
+                       kpi,
+                       "agency",
+                       agency_id,
+                       spatial_aggregation_level,
+                       sep = "/")
+
+  query_parameters = paste(paste0("aggregated_by_time=", tolower(aggregated_by_time)),
+                           paste0("aggregated_by_day=", tolower(aggregated_by_day)),
+                           paste0("included_date_perimeters=",
+                                  paste(start_date,
+                                        end_date,
+                                        days_of_the_week,
+                                        sep = "_")
+                           ),
+                           sep="&")
+
+  kpi_route = paste(kpi_base_url,query_parameters, sep = "?")
+
+  response = getResponseFromRoute(kpi_route, session_id)
+
+  kpi_data_table = do.call(rbind.data.frame, c(response$data, stringsAsFactors = F))
+
+  return(kpi_data_table)
+
+}
+
+
+getCoursesData = function(base_url, service_date = Sys.Date()-7, session_id){
+
+  courses_route = paste0(base_url,"/rest/courses?service_date=",service_date)
+
+  response = getResponseFromRoute(courses_route, session_id)
+
+  courses_data = data.table::rbindlist(response$data, fill = TRUE)
+
+}
+
+getPredictedCoursesData = function(base_url, weekday = 0, session_id){
+
+  predicted_courses_route = paste0(base_url,"/rest/predicted_courses?weekday=",weekday)
+
+  response = getResponseFromRoute(courses_route, session_id)
+  response = jsonlite::fromJSON(response)
+
+  predicted_courses_data = do.call(rbind,response$data$stoptimes)
+
+}
+
+
+######################
+
+# Samples
+#
+# session_id = getSessionId(login, password, group = "lorient", env = "staging")
+#
+# base_url = buildBaseUrl(group = "lorient", env = "staging")
+#
+# agency_id = getAgencyId(base_url, session_id)
+#
+# lines_referential = getReferentialSection(base_url,session_id,"lines")
+#
+# num_courses = getKPIdata(base_url,"num_courses",agency_id, session_id = session_id)
