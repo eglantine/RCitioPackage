f9d8d8a18da30cf3195a37e13bb258b3af90b134
Eglantine Schmitt <eglantine@cit.io>
2022-03-16 10:31:59 +0100

    Fix loop and rename functions

--- R/get_cloud_data.R
+++ R/get_cloud_data.R
@@ -19,23 +19,24 @@ getFileList = function(base_url, session_id, path){
   file_list = unlist(response$data)
 }
 
-getFileFromCloud = function(base_url, session_id, path){
+getFileData = function(base_url, session_id, path){
   gcloud_base_url =  paste0(base_url, "/rest/gcloud/blob?path=")
 
   full_query = paste0(gcloud_base_url,path)
   df = getResponseFromRoute(full_query, session_id)
 }
 
-getFileListFromCloud = function(base_url, session_id, path){
+getFileListData = function(base_url, session_id, path){
   file_list = getFileList(base_url, session_id, path)
 
   i = 1
   data = data.frame()
 
-  for(i in 1:file_list){
+  for(i in 1:length(file_list)){
     data_item = getFileFromCloud(base_url, session_id, file_list[i])
     data = rbind(data, data_item)
     i = i+1
   }
+  return(data)
 
 }
