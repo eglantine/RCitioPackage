832d8ab762da01e8318b62c16ef35ebcf1b8adf2
Eglantine Schmitt <eglantine@cit.io>
2022-03-16 09:45:15 +0100

    Add function to retrieve data from cloud

---
+++ R/get_cloud_data.R
@@ -0,0 +1,41 @@
+getFolderList = function(base_url, session_id, path, depth){
+  gcloud_base_url =  paste0(base_url, "/rest/gcloud/folders")
+
+  query_parameters = paste(paste0("path=",path),
+                           paste0("depth=",depth),
+                           sep="&")
+  full_query = paste(gcloud_base_url,query_parameters, sep = "?")
+  response = getResponseFromRoute(full_query, session_id)
+
+  folder_list = unlist(response$data)
+}
+
+getFileList = function(base_url, session_id, path){
+  gcloud_base_url =  paste0(base_url, "/rest/gcloud/files?path=")
+
+  full_query = paste0(gcloud_base_url,path)
+  response = getResponseFromRoute(full_query, session_id)
+
+  file_list = unlist(response$data)
+}
+
+getFileFromCloud = function(base_url, session_id, path){
+  gcloud_base_url =  paste0(base_url, "/rest/gcloud/blob?path=")
+
+  full_query = paste0(gcloud_base_url,path)
+  df = getResponseFromRoute(full_query, session_id)
+}
+
+getFileListFromCloud = function(base_url, session_id, path){
+  file_list = getFileList(base_url, session_id, path)
+
+  i = 1
+  data = data.frame()
+
+  for(i in 1:file_list){
+    data_item = getFileFromCloud(base_url, session_id, file_list[i])
+    data = rbind(data, data_item)
+    i = i+1
+  }
+
+}
