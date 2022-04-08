8d1fa7b616fec816a70cd12433b4184dc9636239
Eglantine Schmitt <eglantine@cit.io>
2022-04-08 15:56:00 +0200

    Use cloud function for decode ids function

--- R/decode_data.R
+++ R/decode_data.R
@@ -1,8 +1,8 @@
 
 addReferentialNames = function(base_url, session_id, data, id_column_in_data,
                                coded_model, named_model, key_in_coded_model, key_in_named_model, name_column) {
-  coded_model_df = getReferentialSection(base_url,session_id,coded_model)
-  named_model_df = getReferentialSection(base_url,session_id,named_model)
+  coded_model_df = getCloudReferentialSection(base_url, session_id, coded_model)
+  named_model_df = getCloudReferentialSection(base_url, session_id, named_model)
 
   referential = merge(coded_model_df,
                       named_model_df,
