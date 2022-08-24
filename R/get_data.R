347307a46213aae98ccbb6e1968121a33cc82d55
Eglantine Schmitt <eglantine@cit.io>
2022-08-24 18:33:00 +0200

    [Verified] Move functions, add log to base GET function

--- R/get_data.R
+++ R/get_data.R
@@ -15,18 +15,20 @@ buildBaseUrl = function(agency, project, env) {
     ifelse(env=="staging","staging.",""),
     "cit.io"
   )
-  
+
   return(base_url)
 }
 
 getResponseFromRoute = function(route_url, session_id){
-  
+
   if(is.null(session_id)){
     print("No session id")
   } else {
-    
+
+    print(paste("Querying URL:", route_url))
+
     response = GET(route_url,set_cookies(sessionid = session_id))
-    
+
     if(response$status_code == "401"){
       print("Invalid credentials")
     }
@@ -39,7 +41,7 @@ getResponseFromRoute = function(route_url, session_id){
 getAgencyId = function(base_url, session_id){
   agency_route = paste0(base_url, "/rest/agency")
   response = getResponseFromRoute(agency_route,session_id)
-  
+
   if(!is.null(response$detail)){
     stop(response$detail)
   } else {
@@ -54,12 +56,12 @@ getReferentialSection = function(base_url,session_id, referential_section){
   referential_table = lapply(response,clean_referential)
   #  referential_table = data.table::rbindlist(response, fill = TRUE)
   referential_table = data.table::rbindlist(referential_table, fill = TRUE)
-  
+
   return(referential_table)
 }
 
 getKPIdata = function(base_url, kpi, agency_id, spatial_aggregation_level = "line", aggregated_by_time = FALSE,aggregated_by_day = FALSE, start_date = Sys.Date() - 7, end_date = Sys.Date(), days_of_the_week =  1111111, session_id){
-  
+
   kpi_base_url = paste(base_url,
                        "kpis",
                        kpi,
@@ -67,7 +69,7 @@ getKPIdata = function(base_url, kpi, agency_id, spatial_aggregation_level = "lin
                        agency_id,
                        spatial_aggregation_level,
                        sep = "/")
-  
+
   query_parameters = paste(paste0("aggregated_by_time=", tolower(aggregated_by_time)),
                            paste0("aggregated_by_day=", tolower(aggregated_by_day)),
                            paste0("included_date_perimeters=",
@@ -77,141 +79,37 @@ getKPIdata = function(base_url, kpi, agency_id, spatial_aggregation_level = "lin
                                         sep = "_")
                            ),
                            sep="&")
-  
+
   kpi_route = paste(kpi_base_url,query_parameters, sep = "?")
-  
+
   response = getResponseFromRoute(kpi_route, session_id)
-  
+
   kpi_data_table = suppressWarnings(data.table::rbindlist(response$data, fill = TRUE))
 
   return(kpi_data_table)
-  
+
 }
 
 getCoursesData = function(base_url, service_date = Sys.Date()-7, session_id){
-  
+
   courses_route = paste0(base_url,"/rest/courses?service_date=",service_date)
-  
+
   response = getResponseFromRoute(courses_route, session_id)
-  
+
   courses_data = data.table::rbindlist(response$data, fill = TRUE)
-  
+
 }
 
 
 getManualCoursesData = function(base_url, service_date = Sys.Date()-7, session_id){
-  
+
   courses_route = paste0(base_url,"/rest/manual_courses?service_date=",service_date)
-  
+
   response = getResponseFromRoute(courses_route, session_id)
-  
+
   courses_data = data.table::rbindlist(response$data, fill = TRUE)
-  
-}
 
-getPredictedCoursesData = function(base_url, weekday = 0, session_id){
-  
-  predicted_courses_route = paste0(base_url,"/rest/predicted_courses?weekday=",weekday)
-  
-  response = getResponseFromRoute(courses_route, session_id)
-  response = fromJSON(response)
-  
-  predicted_courses_data = do.call(rbind,response$data$stoptimes)
-  
 }
-
-getPredictedOccupancyData = function(base_url,session_id, service_date, granularity = "15_minutes"){
-  predicted_occupancy_route = paste0(base_url,
-                                     "/rest/predicted_occupancy",
-                                     "?service_date=",
-                                     service_date,
-                                     "&granularity=",
-                                     granularity)
-  
-  response = getResponseFromRoute(predicted_occupancy_route, session_id)
-  
-  predicted_occupancy_data = data.table::rbindlist(response$data, fill = TRUE)
-  
-  return(predicted_occupancy_data)
-}
-
-getAllPredictedOccupanciesData = function(base_url,session_id, service_date, granularity = "15_minutes"){
-  predicted_occupancy_route = paste0(base_url,
-                                     "/rest/predicted_occupancy",
-                                     "?service_date=",
-                                     service_date,
-                                     "&granularity=",
-                                     granularity,
-                                     "&show_all_occupancy=True"
-  )
-  
-  response = getResponseFromRoute(predicted_occupancy_route, session_id)
-  
-  predicted_occupancy_data = data.table::rbindlist(response$data, fill = TRUE)
-  
-  return(predicted_occupancy_data)
-}
-
-getPredictedStoptimesData = function(base_url,session_id, service_date){
-  predicted_occupancy_route = paste0(base_url,
-                                     "/rest/predicted_stoptimes",
-                                     "?service_date=",
-                                     service_date
-                                     # ,if(station_id>0){
-                                     # paste0("&station_id=", station_id)
-                                     #   }
-  )
-  
-  response = getResponseFromRoute(predicted_occupancy_route, session_id)
-  
-  predicted_stoptimes_data = data.table::rbindlist(response, fill = TRUE)
-  
-  return(predicted_stoptimes_data)
-}
-
-getServiceDates =function (base_url,session_id){
-  referential_route = paste0(base_url,"/rest/service_date")
-  response = getResponseFromRoute(referential_route, session_id)
-  service_dates = data.table::rbindlist(response, fill = TRUE)
-  
-  return(service_dates)
-}
-
-getMaxServiceDate = function(base_url,session_id){
-  service_dates = getServiceDates(base_url, session_id)
-  max_service_date = max(service_dates[service_dates$num_courses>0,]$service_date)
-  
-  return (max_service_date)
-}
-
-getMaxValidationDate = function(base_url,session_id){
-  service_dates = getServiceDates(base_url, session_id)
-  max_validation_date = max(service_dates[service_dates$num_validations>0,]$service_date)
-  
-  return (max_validation_date)
-}
-
-getMaxCountingCellDate = function(base_url,session_id){
-  service_dates = getServiceDates(base_url, session_id)
-  max_validation_date = max(service_dates[service_dates$num_courses_with_counting_cells>0,]$service_date)
-  
-  return (max_validation_date)
-}
-
-getMaxControlsDate = function(base_url,session_id){
-  service_dates = getServiceDates(base_url, session_id)
-  max_controls_date = max(service_dates[service_dates$num_controls>0,]$service_date)
-  
-  return (max_controls_date)
-}
-
-getMaxScheduledCoursesDate = function(base_url,session_id){
-  service_dates = getServiceDates(base_url, session_id)
-  max_scheduled_courses_date = max(service_dates[service_dates$num_scheduled_courses>0,]$service_date)
-  
-  return (max_scheduled_courses_date)
-}
-
 
 getAgencyConfiguration =function (gateway_base_url,session_id){
   agency_configuration_route = paste0(gateway_base_url,"/agency.json")
@@ -223,19 +121,5 @@ getActiveAgencies = function(){
   agency_list =  sapply(response, function(x) x$name)
   state =  sapply(response, function(x) x$state)
   active_agency_list = agency_list[state == "live"]
-  
+
 }
-
-######################
-
-# Samples
-#
-# session_id = getSessionId(login, password, agency = "lorient", env = "staging")
-#
-# base_url = buildBaseUrl(agency = "lorient","api", env = "staging")
-#
-# agency_id = getAgencyId(base_url, session_id)
-#
-# lines_referential = getReferentialSection(base_url,session_id,"lines")
-#
-# num_courses = getKPIdata(base_url,"num_courses",agency_id, session_id = session_id)
