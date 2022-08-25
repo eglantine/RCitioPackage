6f51a092877f2e43dca266b212b5213cc2252753
Eglantine Schmitt <eglantine@cit.io>
2022-08-25 20:13:43 +0200

    New prediction function

--- R/get_prediction.R
+++ R/get_prediction.R
@@ -42,6 +42,33 @@ getAllPredictedOccupanciesData = function(base_url,session_id, service_date, gra
   return(predicted_occupancy_data)
 }
 
+getPredictedOccupancyData2 = function(base_url, session_id, service_date, granularity = "15_minutes", 
+                                      show_all_occupancy = FALSE, version = "v1"){
+  query_parameters = paste(paste0("service_date=",service_date),
+                           paste0("granularity=", granularity),
+                           sep="&")
+  
+  if(show_all_occupancy == TRUE){
+    query_parameters = paste(query_parameters,"show_all_occupancy=True", sep = "&")
+  }
+  
+  if(version == "v2"){
+    query_parameters = paste(query_parameters,
+                             paste0("version=", version),
+                             sep = "&")
+  }
+  
+  predicted_occupancy_route = paste0(base_url,
+                                     paste("/rest/predicted_occupancy", query_parameters, sep = "?")
+  )
+  
+  response = getResponseFromRoute(predicted_occupancy_route, session_id)
+  
+  predicted_occupancy_data = data.table::rbindlist(response$data, fill = TRUE)
+  
+  return(predicted_occupancy_data)
+}
+
 getPredictedStoptimesData = function(base_url,session_id, service_date){
   predicted_occupancy_route = paste0(base_url,
                                      "/rest/predicted_stoptimes",
